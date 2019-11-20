5b7208f3abfc4493d6f0bd1fedb6991aba62a57b
Alencar <alencar.costa@inf.ufrgs.br>
2019-11-20 14:51:05 -0300

    Corrigindo configuração variavel ambiente DATABASE

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config'
 
 const DBConfig =  {
-  database: process.env.NAME,
-  username: process.env.USER,
-  password: process.env.PASS,
-  host: process.env.HOST, 
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST, 
   dialect: "postgres",
   define: {
     timestamps: true,
--- src/config/databaseOldStyle.js
+++ src/config/databaseOldStyle.js
@@ -2,10 +2,10 @@ require("dotenv/config");
 // This file is needed for Migrations work. Migrations still don't understand ES6 syntax
 module.exports = {
   dialect: "postgres",
-  host: process.env.HOST,
-  username: process.env.USER,
-  password: process.env.PASS,
-  database: process.env.NAME,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
